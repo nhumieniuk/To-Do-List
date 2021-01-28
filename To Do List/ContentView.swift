b4c8990e20f57b956b75500b56159684cd335456
Natan Humieniuk <nhumieniuk@bsd220.org>
2021-01-28 10:29:24 -0600

    Initial Commit

---
+++ To Do List/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  To Do List
+//
+//  Created by Student on 1/28/21.
+//  Copyright © 2021 elenator. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
